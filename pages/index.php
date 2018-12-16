1e9fd758f7dbbd86f9f94e27397b45328676f9ef
David Day <dj6082013@gmail.com>
2018-12-16 17:40:17 +0800

    Make button large in home page

--- pages/index.php
+++ pages/index.php
@@ -2,10 +2,10 @@
 <div class="pure-g" style="text-align: center;">
 	<div class="pure-u-1"><h1>Which one is your role in the contest?</h1></div>
 	<div class="pure-u-1 pure-u-md-1-2">
-		<a class="pure-button" href="/auth?role=participant">Participant</a>
+		<a class="pure-button large-button" href="/auth?role=participant">Participant</a>
 	</div>
 	<div class="pure-u-1 pure-u-md-1-2">
-		<a class="pure-button" href="/auth?role=judge">Judge</a>
+		<a class="pure-button large-button" href="/auth?role=judge">Judge</a>
 	</div>
 </div>
 <? show_footer(); ?>
