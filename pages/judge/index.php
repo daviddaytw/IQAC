f8b316291a2b5d766721b560e80a1bb0d545bfaf
David Day <dj6082013@gmail.com>
2018-12-17 19:30:19 +0800

    Minor bug fix

--- pages/judge/index.php
+++ pages/judge/index.php
@@ -25,18 +25,18 @@ if(isset($_GET['contest'])){
 	}
 }
 
-// Get participants of contest
-$participants = array();
-if ($stmt = $db->prepare("SELECT * FROM `Participants` WHERE CONTEST=?")) {
-	$stmt->bind_param("i",$_SESSION['CONTEST']['ID']);
-	$stmt->execute();
-	$result = $stmt->get_result();
-	while( ($row=$result->fetch_assoc()) != NULL ) $participants[$row['ID']] = $row['NAME'];
-	$result->free();
-	$stmt->close();
-} else die('Error while preparing SQL');
-
 if(isset($_SESSION['CONTEST'])){
+	// Get participants of contest
+	$participants = array();
+	if ($stmt = $db->prepare("SELECT * FROM `Participants` WHERE CONTEST=?")) {
+		$stmt->bind_param("i",$_SESSION['CONTEST']['ID']);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		while( ($row=$result->fetch_assoc()) != NULL ) $participants[$row['ID']] = $row['NAME'];
+		$result->free();
+		$stmt->close();
+	} else die('Error while preparing SQL');
+
 	show_header($_SESSION['CONTEST']['NAME'] , 'ScoreBoard');
 	echo '<div class="panel">';
 	echo '<p>This contest start at '.$_SESSION['CONTEST']['BEGIN'].', finish at '.$_SESSION['CONTEST']['FINISH'].'</p>';
