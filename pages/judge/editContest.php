7b1b52e5605173c851e49382c62bbf06cfec6d8e
David Day <dj6082013@gmail.com>
2019-01-20 16:33:33 +0800

    Multi-judge control  form

--- pages/judge/editContest.php
+++ pages/judge/editContest.php
@@ -32,36 +32,99 @@ if(isset($_POST['name'])){
 			$stmt->close();
 		} else die('Error while preparing SQL');
 	}
-	
+}
+
+// edit judge link
+if(isset($_POST['judge'],$CONTEST_INFO)){
+	if($_POST['action'] == 'add'){
+		if ($stmt = $db->prepare("INSERT INTO `Judges` (`JUDGE`,`CONTEST`) VALUES (?,?);")) {
+			$stmt->bind_param("si",$_POST['judge'],$CONTEST_INFO['ID']);
+			$stmt->execute();
+			$stmt->close();
+		} else die('Error while preparing SQL');
+	}
+	if($_POST['action'] == 'remove'){
+		if ($stmt = $db->prepare("DELETE FROM `Judges`  WHERE `JUDGE`=? AND `CONTEST`=?")) {
+			$stmt->bind_param("si",$_POST['judge'],$CONTEST_INFO['ID']);
+			$stmt->execute();
+			$stmt->close();
+		} else die('Error while preparing SQL');
+	}
 }
 ?>
-<div style="text-align: center;">
-	<form class="pure-form pure-form-aligned" method="POST">
-		<fieldset>
-			<div class="pure-control-group">
-				<label for="name">Contest Name</label>
-				<input id="name" name="name" type="text" value="<?= $CONTEST_INFO['NAME'] ?>" required>
-			</div>
-			<div class="pure-control-group">
-				<label for="beginDate">Begin Date</label>
-				<input id="beginDate" name="beginDate" type="date" value="<?= explode(' ',$CONTEST_INFO['BEGIN'])[0] ?>" required>
-			</div>
-			<div class="pure-control-group">
-				<label for="beginTime">Begin Time</label>
-				<input id="beginTime" name="beginTime" type="time" value="<?= explode(' ',$CONTEST_INFO['BEGIN'])[1] ?>" required>
-			</div>
-			<div class="pure-control-group">
-				<label for="finishDate">Finish Date</label>
-				<input id="finishDate" name="finishDate" type="date" value="<?= explode(' ',$CONTEST_INFO['FINISH'])[0] ?>" required>
-			</div>
-			<div class="pure-control-group">
-				<label for="finishTime">Finish Time</label>
-				<input id="finishTime" name="finishTime" type="time" value="<?= explode(' ',$CONTEST_INFO['FINISH'])[1] ?>" required>
-			</div>
-			<div class="pure-controls">
-				<button type="submit" class="pure-button pure-button-primary">Submit</button>
-			</div>
-		</fieldset>
-	</form>
+<div class="pure-g">
+	<div class="pure-u-1 pure-u-md-1-2">
+		<form class="pure-form pure-form-aligned" method="POST">
+			<fieldset>
+				<div class="pure-control-group">
+					<label for="name">Contest Name</label>
+					<input id="name" name="name" type="text" value="<?= $CONTEST_INFO['NAME'] ?>" required>
+				</div>
+				<div class="pure-control-group">
+					<label for="beginDate">Begin Date</label>
+					<input id="beginDate" name="beginDate" type="date" value="<?= explode(' ',$CONTEST_INFO['BEGIN'])[0] ?>" required>
+				</div>
+				<div class="pure-control-group">
+					<label for="beginTime">Begin Time</label>
+					<input id="beginTime" name="beginTime" type="time" value="<?= explode(' ',$CONTEST_INFO['BEGIN'])[1] ?>" required>
+				</div>
+				<div class="pure-control-group">
+					<label for="finishDate">Finish Date</label>
+					<input id="finishDate" name="finishDate" type="date" value="<?= explode(' ',$CONTEST_INFO['FINISH'])[0] ?>" required>
+				</div>
+				<div class="pure-control-group">
+					<label for="finishTime">Finish Time</label>
+					<input id="finishTime" name="finishTime" type="time" value="<?= explode(' ',$CONTEST_INFO['FINISH'])[1] ?>" required>
+				</div>
+				<div class="pure-controls">
+					<button type="submit" class="pure-button pure-button-primary">Submit</button>
+				</div>
+			</fieldset>
+		</form>
+	</div>
+	<? if(isset($CONTEST_INFO)): ?>
+		<div class="pure-u-1 pure-u-md-1-2">
+			<table class="pure-table">
+				<thead>
+					<tr><th>Judge ID list</th></tr>
+				</thead>
+				<tbody>
+					<?
+					if ($stmt = $db->prepare("SELECT * FROM `Judges` WHERE CONTEST=?")) {
+						$stmt->bind_param("i",$CONTEST_INFO['ID']);
+						$stmt->execute();
+						$result = $stmt->get_result();
+
+						while( $row = $result->fetch_assoc() ){
+							echo '<tr>';
+							echo '<td>'.$row['JUDGE'].'</td>';
+							echo '</tr>';
+						}
+						$result->free();
+						$stmt->close();
+					} else die('Error while preparing SQL');
+					?>
+				</tbody>
+			</table>
+			<form class="pure-form pure-form-aligned" method="POST">
+				<fieldset>
+					<div class="pure-control-group">
+						<label for="judge">Judge ID</label>
+						<input id="judge" name="judge" type="text" required>
+					</div>
+					<div class="pure-control-group">
+						<label for="action">Action</label>
+						<select id="action" name="action">
+							<option value="add">Add</option>
+							<option value="remove">Remove</option>
+						</select>
+					</div>
+					<div class="pure-controls">
+						<button type="submit" class="pure-button pure-button-primary">Submit</button>
+					</div>
+				</fieldset>
+			</form>
+		</div>
+	<? endif; ?>
 </div>
 <? show_footer(); ?>
