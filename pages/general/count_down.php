04798187e8e5b1e94c4b2240c7c14e3f06a7d764
David Day <dj6082013@gmail.com>
2019-04-14 20:21:29 +0800

    Create link for contest (File missed)

--- pages/general/count_down.php
+++ pages/general/count_down.php
@@ -1,14 +1,22 @@
 <h1>Contest has not start yet!!</h1>
+
 <? if($_SESSION['ROLE'] == 'judge'): ?>
-	<h2>Contest&nbsp;ID:&nbsp;<?= $CONTEST_INFO['ID'] ?></h2>
+<h2>
+	<a href="https://<?= $_SERVER['REQUEST_URI'] ?>/auth?Contest=<?= $CONTEST_INFO['ID'] ?>">
+		Contest ID: <?= $CONTEST_INFO['ID'] ?>
+	</a>
+</h2>
 <? endif; ?>
+
 <h2 id="count_down"><?= strtotime($CONTEST_INFO['BEGIN'])-time() ?></h2>
 <div class="pure-g" style="text-align: center;">';
+
 <? foreach(getPartcipantsOfContest($CONTEST_INFO['ID']) as $name): ?>
 	<div class="pure-u-1-3 pure-u-md-1-6">
 		<p><?= htmlentities($name) ?></p></div>
 	</div>
 <? endforeach; ?>
+
 <script>
 	var timeleft = document.getElementById("count_down").innerHTML;
 	setTimeout(function(){ location = '' }, 20000);
