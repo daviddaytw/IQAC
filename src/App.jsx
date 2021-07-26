af2f2f712e8f096cfef939c8461d9567c6b4ef5c
David Day <dj6082013@gmail.com>
2021-07-26 11:37:23 +0800

    Add account page.

--- src/App.jsx
+++ src/App.jsx
@@ -6,57 +6,90 @@ import {
   Link,
 } from 'react-router-dom';
 import Parse from 'parse';
-import { Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  AppBar, Avatar, Button, Toolbar, Typography,
+} from '@material-ui/core';
+import Account from './pages/account';
 import ContestList from './pages/contest-list';
 import Contest from './pages/contest';
 import Home from './pages/home';
 import Privacy from './pages/privacy';
 
-const headerStyle = {
-  color: '#888',
-  fontSize: '1.25rem',
-  lineHeight: 2,
-  marginLeft: '1rem',
-  textTransform: 'uppercase',
-  textDecoration: 'none',
-};
-
-const footerStyle = {
-  textAlign: 'center',
-  marginTop: '1rem',
-};
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  link: {
+    color: 'inherit',
+    textDecoration: 'inherit',
+    cursor: 'inherit',
+  },
+  brand: {
+    color: 'inherit',
+    textDecoration: 'inherit',
+    cursor: 'inherit',
+    flexGrow: 1,
+  },
+  footer: {
+    textAlign: 'center',
+    marginTop: '1rem',
+  },
+}));
 
 function App() {
-  const [loginStatus, setLoginStatus] = useState(false);
+  const classes = useStyles();
+  const [userInfo, setUserInfo] = useState();
 
   useEffect(() => {
     Parse.User.currentAsync()
       .then((user) => {
-        if (user) setLoginStatus(true);
+        if (user) {
+          setUserInfo({
+            name: user.get('username'),
+            avatarUrl: user.get('avatarUrl'),
+          });
+        }
       });
   }, []);
 
   return (
     <Router>
-      <header>
-        <Box><Link to="/" style={headerStyle}>Instant Q&amp;A Contest</Link></Box>
-      </header>
+      <AppBar position="static">
+        <Toolbar>
+          <Link className={classes.brand} to="/">
+            <Typography variant="h6">Instant Q&amp;A Contest</Typography>
+          </Link>
+          { userInfo && (
+            <Link className={classes.link} to="/account">
+              <Button color="inherit">
+                {userInfo.name}
+                <Avatar alt={userInfo.name} src={userInfo.avatarUrl} />
+              </Button>
+            </Link>
+          )}
+        </Toolbar>
+      </AppBar>
       <Switch>
         <Route exact path="/">
-          { loginStatus ? <ContestList /> : <Home /> }
+          { userInfo ? <ContestList /> : <Home /> }
         </Route>
         <Route exact path="/privacy-policy">
           <Privacy />
         </Route>
-        { loginStatus && (
-        <>
+        { userInfo && (
+          <Route exact path="/account">
+            <Account />
+          </Route>
+        )}
         <Route path="/:id">
           <Contest />
         </Route>
-        </>
-        )}
       </Switch>
-      <footer style={footerStyle}>
+      <footer className={classes.footer}>
         Code, Wiki and Discussion of IQAC is available at&nbsp;
         <a href="https://github.com/dj6082013/IQAC/">Github</a>
       </footer>
