3ec267390f2356599cca10caf9e3f3155ae60352
David Day <dj6082013@gmail.com>
2021-07-26 09:59:20 +0800

    Add Privacy Policy

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import { Box } from '@material-ui/core';
 import ContestList from './pages/contest-list';
 import Contest from './pages/contest';
 import Home from './pages/home';
+import Privacy from './pages/privacy';
 
 const headerStyle = {
   color: '#888',
@@ -44,9 +45,16 @@ function App() {
         <Route exact path="/">
           { loginStatus ? <ContestList /> : <Home /> }
         </Route>
+        <Route exact path="/privacy-policy">
+          <Privacy />
+        </Route>
+        { loginStatus && (
+        <>
         <Route path="/:id">
           <Contest />
         </Route>
+        </>
+        )}
       </Switch>
       <footer style={footerStyle}>
         Code, Wiki and Discussion of IQAC is available at&nbsp;
