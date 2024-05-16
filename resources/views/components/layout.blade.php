405c502b871d70c1d44f7fff55d545236ea76983
David Day <dj6082013@gmail.com>
2024-05-15 20:21:08 -0700

    Add author info.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,6 +11,10 @@
     </head>
     <body>
         {{ $slot }}
+        <footer class="p-4 d-flex gap-2 justify-content-end">
+            <div class="text-secondary">Built with ❤️ by <a href="https://davidday.tw" class="text-decoration-none" target="_blank">David Day</a></div>
+            <iframe src="https://ghbtns.com/github-btn.html?user=daviddaytw&repo=IQAC&type=star&count=true" frameborder="0" scrolling="0" width="170" height="30" title="GitHub"></iframe>
+        </footer>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
 </html>
