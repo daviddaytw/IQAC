17d0923c6fc94887f367700498ed930984b57c60
David Day <dj6082013@gmail.com>
2024-05-15 18:48:33 -0700

    Add Continuous Delivery.

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo "Putting the website into maintenance mode..."
+php artisan down
+
+echo "Running composer..."
+rm -rf ./vendor
+/opt/cpanel/composer/bin/composer install --optimize-autoloader --no-dev
+
+echo "Linking storage..."
+php artisan storage:link
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Caching views..."
+php artisan view:cache
+
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "Putting the website back online..."
+php artisan up
